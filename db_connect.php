8aac2ae39098c4d5191d95815c574b6d2ab37999
julianasalvatore <julianalirarj@gmail.com>
2024-09-24 16:20:49 -0300

    att

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Altere conforme necessário
 $username = "root"; // Altere conforme necessário
 $password = ""; // Altere conforme necessário
-$dbname = "nome_do_banco"; // Altere conforme necessário
+$dbname = "prefeitura"; // Nome do banco de dados
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Checa a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
+} else {
+    echo "Conexão bem-sucedida!";
 }
 ?>
