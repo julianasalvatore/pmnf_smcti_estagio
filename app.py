8817d70ae5f66b8f5a901321447f65f2a57a76ce
julianasalvatore <julianalirarj@gmail.com>
2024-09-24 22:29:30 -0300

    d

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import os
 
 app = Flask(__name__)
 
+# Rota para servir arquivos
 @app.route('/<path:filename>')
 def serve_file(filename):
+    # Serve arquivos do diretório especificado
     return send_from_directory('sistema_estagio_2024/pmnf_smcti_estagio', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9091)
+    # Executa o servidor na porta 9090
+    app.run(host='0.0.0.0', port=9090)
